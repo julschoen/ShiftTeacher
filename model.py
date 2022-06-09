56a7b239ef3db7fb0dfd49d355679860e785654f
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-09 14:59:44 +0200

    init

---
+++ model.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+
+class ShiftTeacher(nn.Module):
+    def __init__(self, params):
+        super(LeNetShiftPredictor, self).__init__()
+        width = params.filters
+        self.convnet = nn.Sequential(
+            nn.Conv2d(1 * 2, 3 * width, kernel_size=(5, 5)),
+            nn.BatchNorm2d(3 * width),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(3 * width, 8 * width, kernel_size=(5, 5)),
+            nn.BatchNorm2d(8 * width),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(8 * width, 60 * width, kernel_size=(5, 5)),
+            nn.BatchNorm2d(60 * width),
+            nn.ReLU()
+        )
+
+        self.fc_shift = nn.Sequential(
+            nn.Linear(60 * width, 42 * width),
+            nn.BatchNorm1d(42 * width),
+            nn.ReLU(),
+            nn.Linear(42 * width, 1)
+        )
+
+    def forward(self, x1, x2):
+        batch_size = x1.shape[0]
+        features = self.convnet(torch.cat([x1, x2], dim=1))
+
+        features = features.mean(dim=[-1, -2])
+        features = features.view(batch_size, -1)
+        shift = self.fc_shift(features)
+
+        return shift.squeeze()
---
+++ trainer.py
@@ -0,0 +1,137 @@
+import os
+import numpy as np
+import pickle
+
+import torch 
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.cuda.amp import autocast, GradScaler
+
+from model import ShiftTeacher
+
+
+class Trainer(object):
+    def __init__(self, dataset, params):
+        ### Misc ###
+        self.p = params
+        self.device = params.device
+
+        ### Make Dirs ###
+        self.log_dir = params.log_dir
+        os.makedirs(self.log_dir, exist_ok=True)
+
+        ### load/save params
+        if params.load_params:
+            with open(os.path.join(params.log_dir, 'params.pkl'), 'rb') as file:
+                params = pickle.load(file)
+        else:
+            with open(os.path.join(params.log_dir,'params.pkl'), 'wb') as file:
+                pickle.dump(params, file)
+
+        ### Make Models ###
+        self.model = ShiftTeacher(self.p).to(self.device)
+        
+        if self.p.ngpu>1:
+            self.model = nn.DataParallel(self.model)
+
+        self.opt = optim.Adam(self.model.parameters(), lr=self.p.lr)
+        
+        self.grad_scaler = GradScaler()
+
+        ### Make Data Generator ###
+        self.generator_train = DataLoader(dataset, batch_size=self.p.batch_size, shuffle=True, num_workers=4, drop_last=True)
+
+        ### Prep Training
+        self.losses = []
+        self.val_losses = []
+
+    def inf_train_gen(self):
+        while True:
+            for data in self.generator_train:
+                yield data
+
+    def start_from_checkpoint(self):
+        step = 0
+        files = [f for f in os.listdir(self.log_dir)]
+        if len(files) < 2:
+            checkpoint = os.path.join(self.log_dir, 'checkpoint.pt')
+        else:
+            files.remove('checkpoint.pt')
+            files = sorted(files, key=lambda x: int(x.split('_')[1].split('.')[0]))
+            checkpoint = os.path.join(self.log_dir, files[-1])
+
+        if os.path.isfile(checkpoint):
+            state_dict = torch.load(checkpoint)
+            step = state_dict['step']
+            self.model.load_state_dict(state_dict['model'])
+            self.opt.load_state_dict(state_dict['opt'])
+            self.losses = state_dict['loss']
+            self.val_losses = state_dict['val_loss']
+            print('starting from step {}'.format(step))
+        return step
+
+    def save_checkpoint(self, step):
+        if step < self.p.niters - 1001:
+            name = 'checkpoint.pt'
+        else:
+            name = f'checkpoint_{step}.pt'
+
+        torch.save({
+        'step': step,
+        'model': self.model.state_dict(),
+        'opt': self.opt.state_dict(),
+        'loss': self.losses,
+        'val_loss': self.val_losses,
+        }, os.path.join(self.log_dir, name))
+
+    def log(self, step):
+        if step % self.p.steps_per_log == 0:
+            print('[%d/%d] Loss: %.2f, Val Loss'
+                        % (step, self.p.niters, self.losses[-1], self.val_losses[-1]))
+
+        if step % self.p.steps_per_checkpoint == 0:
+            self.save_checkpoint(step)
+
+    def log_final(self, step):
+        self.log(step)
+        self.save_checkpoint(step)
+
+    def step(self)
+        for p in self.model.parameters():
+                    p.requires_grad = True
+            
+        data, shifts = next(gen)
+        self.model.zero_grad()
+        with autocast():
+            pred = self.model(data)
+
+        loss = torch.mean(torch.abs(shift_prediction - shifts))
+
+        self.grad_scaler.scale(loss).backward()
+        self.grad_scaler.step(self.opt)
+        self.grad_scaler.update()
+
+        for p in self.model.parameters():
+            p.requires_grad = False
+
+        return loss.item()
+
+    def train(self):
+        step_done = self.start_from_checkpoint()
+        gen = self.inf_train_gen()
+
+        print("Starting Training...")
+        for i in range(step_done, self.p.niters):
+            
+            loss = self.step()
+            val_loss = self.val_step()
+
+            self.losses.append(loss)
+            self.val_losses.append(val_loss)
+
+            self.log(i)
+                
+        self.log_final(i)
+
+        print('...Done')
