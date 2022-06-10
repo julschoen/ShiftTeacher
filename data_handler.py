ee2cbf1f3bb36651fbe0ceabc44420e2bf4ea141
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-10 10:18:29 +0200

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -10,15 +10,31 @@ class DATA(Dataset):
     self.len = self.files.shape[0]
 
   def __getitem__(self, index):
+    if np.random.rand() < 0.51:
       x = self.files[index]
       ind = np.sort(np.random.choice(x.shape[0], 2, replace=False))
       while ind[0]+1 == ind[1]:
         ind = np.sort(np.random.choice(x.shape[0], 2, replace=False))
       ind = np.sort(np.append(ind, np.mean(ind).astype(int)))
       xs = x[ind]
-      y = (ind[2]-ind[0])/(x.shape[0]-ind[0])
+      shift = (ind[2]-ind[0])/(x.shape[0]-ind[0])
       xs = np.clip(xs, -1, 1)
-      return torch.from_numpy(xs).float().squeeze(), torch.Tensor([y])
+      same = 1
+    else:
+      x1 = self.files[index]
+      x2 = self.files[np.random.choice(self.len, 1)]
+      num_others = np.random.randint(1,3)
+      ind1 = np.sort(np.random.choice(x1.shape[0], 2, replace=False))
+      ind2 = np.sort(np.random.choice(x2.shape[0], num_others, replace=False))
+      while ind[0]+1 == ind[1]:
+        ind = np.sort(np.random.choice(x1.shape[0], 2, replace=False))
+      ind = np.sort(np.append(ind, np.mean(ind).astype(int)))
+      xs = x1[ind1]
+      xs[np.random.choice(xs.shape[0], num_others, replace=False)] = x2[ind2]
+      shift = (ind1[2]-ind1[0])/(x1.shape[0]-ind1[0])
+      xs = np.clip(xs, -1, 1)
+      same = 0
+    return torch.from_numpy(xs).float().squeeze(), torch.Tensor([y]).float(), torch.Tensor([same]).float()
 
   def __len__(self):
       return self.len
