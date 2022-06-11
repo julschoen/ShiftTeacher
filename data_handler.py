81314aeb2e2e464cc6596e39f5da4e0da89a6893
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-11 11:55:09 +0200

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -17,7 +17,7 @@ class DATA(Dataset):
         ind = np.sort(np.random.choice(x.shape[0], 2, replace=False))
       ind = np.sort(np.append(ind, np.mean(ind).astype(int)))
       xs = x[ind]
-      shift = (ind[2]-ind[0])/(x.shape[0]-ind[0])
+      shift = ind[2]-ind[0]
       xs = np.clip(xs, -1, 1)
       same = 1
     else:
@@ -31,7 +31,7 @@ class DATA(Dataset):
       ind1 = np.sort(np.append(ind1, np.mean(ind1).astype(int)))
       xs = x1[ind1]
       xs[np.random.choice(xs.shape[0], num_others, replace=False)] = x2[ind2]
-      shift = (ind1[2]-ind1[0])/(x1.shape[0]-ind1[0])
+      shift = ind1[2]-ind1[0]
       xs = np.clip(xs, -1, 1)
       same = 0
     return torch.from_numpy(xs).float().squeeze(), torch.Tensor([shift]).float(), torch.Tensor([same]).float()
